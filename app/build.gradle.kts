26f028aa37c4aece2db653bc8490c600117d4459
mtony32-beep <mtony32@gmail.com>
2026-08-28 03:39:28 +0700

    🔧 FIX: Hapus dependensi salah + tambah fragment-ktx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,5 +27,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:bottomnavigationview:1.11.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 }
